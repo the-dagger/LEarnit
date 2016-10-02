8f26c9c9d55eb0e6fcc9250b6b00fb70ed2a894c
Vineet Garg <grgvineet@gmail.com>
2016-10-02 15:48:38 +0530

    Added real wallet amount

--- app/src/main/java/com/example/the_dagger/learnit/activity/WalletActivity.java
+++ app/src/main/java/com/example/the_dagger/learnit/activity/WalletActivity.java
@@ -2,15 +2,58 @@ package com.example.the_dagger.learnit.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.the_dagger.learnit.R;
+import com.example.the_dagger.learnit.paytm.Paytm;
+import com.example.the_dagger.learnit.utility.SuperPrefs;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WalletActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        textView = (TextView) findViewById(R.id.currentBalance);
+
+        textView.setText(SuperPrefs.newInstance(getApplicationContext()).getInt("balance", 0) + " ₹");
+        String access_token = SuperPrefs.newInstance(getApplicationContext()).getString("access_token");
+
+        Map<String, String> header = new HashMap<>();
+        header.put("ssotoken", access_token);
+
+        Paytm paytm = Paytm.getInstance();
+        paytm.checkBalance(header, new JSONObject(), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject res = response.getJSONObject("response");
+                    int balance = res.getInt("amount");
+                    SuperPrefs.newInstance(getApplicationContext()).setInt("balance", balance);
+                    textView.setText(balance + " ₹");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error getting wallet amount", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
