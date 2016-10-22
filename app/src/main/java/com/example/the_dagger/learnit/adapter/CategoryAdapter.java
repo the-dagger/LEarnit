9554121636b638055fc14d50a05e213b029b1fcd
pranaypatel512 <www.pranay1337.com@gmail.com>
2016-10-23 00:17:56 +0530

    refactor: [*] Strings and logcat output code

--- app/src/main/java/com/example/the_dagger/learnit/adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/the_dagger/learnit/adapter/CategoryAdapter.java
@@ -23,9 +23,9 @@ import java.util.List;
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> implements View.OnClickListener {
     private int position;
     private List<Categories> listCategories;
-    ArrayList<SingleChoiceQuestion> singleChoiceQuestionArrayList;
+    private ArrayList<SingleChoiceQuestion> singleChoiceQuestionArrayList;
     private final Context context;
-    int answer[] = new int[10];
+    private int answer[] = new int[10];
     @Override
     public CategoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_category_item,parent,false);
@@ -49,11 +49,11 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         this.position = holder.getAdapterPosition();
         Categories singleCategory = listCategories.get(holder.getAdapterPosition());
         if(getItemCount() == -1){
-            holder.title.setText("No Categories at the moment");
+            holder.title.setText(R.string.str_no_category);
             holder.title.setGravity(View.TEXT_ALIGNMENT_CENTER);
         }
         else{
-//            Log.e("Name",singleCategory.getName());
+//            LogUtils.LOGE("Name",singleCategory.getName());
             holder.title.setText(singleCategory.getName());
         }
     }
@@ -73,12 +73,12 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         context.startActivity(intent);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    class ViewHolder extends RecyclerView.ViewHolder{
 
 
         public TextView title;
 
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.category_title);
         }
